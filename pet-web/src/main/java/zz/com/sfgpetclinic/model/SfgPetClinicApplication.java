26644488edbb0c107774c68543f23cb97b987624
大任 <voyagerzark@gmail.com>
2021-12-08 09:22:26 +0800

    refactoring to multi-module maven project. closes #1

--- pet-web/src/main/java/zz/com/sfgpetclinic/model/SfgPetClinicApplication.java
+++ pet-web/src/main/java/zz/com/sfgpetclinic/model/SfgPetClinicApplication.java
@@ -1,4 +1,4 @@
-package zz.com.sfgpetclinic;
+package zz.com.sfgpetclinic.model;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,3 +11,8 @@ public class SfgPetClinicApplication {
     }
 
 }
+
+
+
+
+
